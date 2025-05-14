9ef36dcc96402d4d31dd421dcbfd8c1761e11987
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-14 21:29:29 +0200

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,9 @@
-<nav>
-    <div class="topnav">
-        <div>
-            <a href="{{ url('/') }}" style="font-weight: bold; font-size: 1.2rem;">
-                <i class="bi bi-cloud-fill"></i> EuropeHost.eu
-            </a>
-        </div>
-        <div>
+<nav class="topnav">
+    <div class="nav-inner">
+        <a href="{{ url('/') }}" class="brand">
+            <i class="bi bi-cloud-fill"></i> EuropeHost.eu
+        </a>
+        <div class="nav-links">
             <a href="{{ route('home') }}">Home</a>
             <a href="{{ route('about') }}">About</a>
             <a href="{{ route('projects') }}">Projects</a>
