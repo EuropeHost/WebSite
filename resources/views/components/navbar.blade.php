3ba5d4cae18d11275e44032c76b19af2d9ac6202
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-14 21:27:34 +0200

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,15 @@
 <nav>
-    <a href="{{ route('home') }}">Home</a> |
-    <a href="{{ route('about') }}">About</a> |
-    <a href="{{ route('projects') }}">Projects</a> |
-    <a href="{{ route('socials') }}">Socials</a>
+    <div class="topnav">
+        <div>
+            <a href="{{ url('/') }}" style="font-weight: bold; font-size: 1.2rem;">
+                <i class="bi bi-cloud-fill"></i> EuropeHost.eu
+            </a>
+        </div>
+        <div>
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('about') }}">About</a>
+            <a href="{{ route('projects') }}">Projects</a>
+            <a href="{{ route('socials') }}">Socials</a>
+        </div>
+    </div>
 </nav>
